e0bf1c5bf6cf89ba2067e841a8ac529e9d6815ae
Eshed <ashdnazg@gmail.com>
2013-01-07 21:44:06 +0200

    Licensing and a bit of readme
    --HG--
    rename : README => README.md

--- python/cal/cal_types.py
+++ python/cal/cal_types.py
@@ -1,6 +1,25 @@
 '''
 @summary: Here's where most of the logic exists. A bunch of classes providing a simple python interface hiding Wireshark's.
 '''
+# cal_types.py
+#
+# Pyreshark Plugin for Wireshark. (https://github.com/pyreshark/PyreShark)
+#
+# Copyright (c) 2013 by Eshed Shaham.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 from ctypes import POINTER, pointer, c_int, c_char, addressof, c_char_p
 from struct import unpack, calcsize
--- python/cal/param_structs.py
+++ python/cal/param_structs.py
@@ -1,6 +1,25 @@
 '''
 @dummary: Declares all the ctypes Param Strcutures. See param_structs.h for the C versions.
 '''
+# param_structs.py
+#
+# Pyreshark Plugin for Wireshark. (https://github.com/pyreshark/PyreShark)
+#
+# Copyright (c) 2013 by Eshed Shaham.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 from ctypes import POINTER, Structure, c_int, c_uint, c_char_p
 from ws_types import WSproto_node, WSproto_tree
